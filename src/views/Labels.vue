6e17645de20820a633b08fab01613d1742e12ae7
luyandan <2103029766@qq.com>
2021-05-08 19:45:46 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Labels.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Labels.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -10,6 +13,16 @@ export default {
 };
 </script>
 
-<style lang="ts" scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
