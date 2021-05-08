bbea5c4665dfc0685cd47b96eff948071b89c2b5
luyandan <2103029766@qq.com>
2021-05-08 18:50:35 +0800

    Money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
+    <div class="nav">
       <router-link to="/money">记账</router-link> |
       <router-link to="/labels">标签</router-link> |
       <router-link to="/statistics">统计</router-link>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -15,5 +13,7 @@
 </script>
 
 <style lang="scss">
-
+.nav {
+  border: 1px solid red;
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,61 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -10,6 +66,16 @@ export default {
 };
 </script>
 
-<style lang="ts" scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
