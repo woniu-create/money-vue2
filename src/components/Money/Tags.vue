ef4d4e807aa6affe741f277c7d47de4fc000d62c
luyandan <2103029766@qq.com>
2021-05-09 23:34:07 +0800

    使用.sync方式实现新增标签

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <!-- <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)" :class="selectedTags.indexOf(tag)>=0 ? 'selected' : ''">{{tag}}</li> -->
@@ -16,7 +16,7 @@ import Vue from 'vue';
 import {Component,Prop} from 'vue-property-decorator'
 @Component
 export default class Tags extends Vue{
-  @Prop() dataSource: string[] | undefined;
+  @Prop() readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
   toggle(tag: string){
    const index = this.selectedTags.indexOf(tag);
@@ -25,6 +25,14 @@ export default class Tags extends Vue{
    }else{
      this.selectedTags.push(tag)
    }
+  }
+  create(){
+    const name = window.prompt('请输入标签名');
+    if(name === ''){
+      window.alert('标签名不能为空');
+    }else if(this.dataSource){
+      this.$emit('update:dataSource',[...this.dataSource,name])
+    }
   }
 }
 </script>
