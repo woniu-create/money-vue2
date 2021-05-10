42ca9155a01d8885b2a862a4f7e99b85b7c2ec40
luyandan <2103029766@qq.com>
2021-05-10 13:45:18 +0800

    将record值保存到localstorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,51 +1,57 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
+    {{ record }}
     <!-- <NumberPad :value="record.amount" @update:value="record.amount=$event"/>
     <Types :value="record.type" @update:value="record.type=$event"/> -->
-    <NumberPad :value.sync="record.amount"/>
-    <Types :value.sync="record.type"/>
-    <Notes @update:value="onUpdateNotes"/>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Types :value.sync="record.type" />
+    <Notes @update:value="onUpdateNotes" />
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
 
 <script lang="ts">
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
-import Tags from '@/components/Money/Tags.vue';
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Tags from "@/components/Money/Tags.vue";
+import Vue from "vue";
+import { Component, Watch } from "vue-property-decorator";
 
 type Record = {
-  tags: string[]
-  notes: string 
-  type: string 
-  amount: number
-}
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+};
 
 @Component({
-components: {NumberPad,Types,Notes,Tags},
+  components: { NumberPad, Types, Notes, Tags },
 })
-export default class Money extends Vue{
-  tags = ['衣','食','住','行']
-  record:Record = {
-   tags: [],notes:'',type:'-',amount:0
-  }
+export default class Money extends Vue {
+  tags = ["衣", "食", "住", "行"];
+  recordList: Record[] = [];
+  record: Record = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
 
-  onUpdateTags(value: string[]){
-   this.record.tags = value
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
   }
-  onUpdateNotes(value: string){
-    this.record.notes = value
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
   }
-  // onUpdateType(value: string){
-  //   this.record.type=value
-  // }
-  // onUpdateAmount(value: string){
-  //   this.record.amount=parseFloat(value)
-  // }
+  saveRecord() {
+    const record2 = JSON.parse(JSON.stringify(this.record));
+    this.recordList.push(record2);
+  }
+  @Watch("recordList")
+  onRecordListChange() {
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+  }
 }
 </script>
 
@@ -55,6 +61,4 @@ export default class Money extends Vue{
   flex-direction: column-reverse;
 }
 </style>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
