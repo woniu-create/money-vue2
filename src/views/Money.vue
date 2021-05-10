d879f23c5f071ef684377f67cb1d5a2250880d37
luyandan <2103029766@qq.com>
2021-05-10 13:59:46 +0800

    添加createAt字段，将数据从localstorage取出

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
+    {{ recordList }}
     <!-- <NumberPad :value="record.amount" @update:value="record.amount=$event"/>
     <Types :value="record.type" @update:value="record.type=$event"/> -->
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
@@ -18,11 +18,14 @@ import Tags from "@/components/Money/Tags.vue";
 import Vue from "vue";
 import { Component, Watch } from "vue-property-decorator";
 
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList')||'[]');
+
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createAt?: Date
 };
 
 @Component({
@@ -30,7 +33,7 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
-  recordList: Record[] = [];
+  recordList: Record[] = recordList;
   record: Record = {
     tags: [],
     notes: "",
@@ -46,6 +49,7 @@ export default class Money extends Vue {
   }
   saveRecord() {
     const record2 = JSON.parse(JSON.stringify(this.record));
+    record2.createAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
