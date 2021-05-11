bd051f68e9dbeb223e7f8fec049bff6a5b85017c
luyandan <2103029766@qq.com>
2021-05-11 12:09:27 +0800

    标签页获取params参数

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -4,11 +4,24 @@
     </Layout>
 </template>
 <script lang="ts">
+import tagListmodel from '@/models/tagListModel';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 @Component
 export default class EditLabel extends Vue {
-    
+  created() {
+    //   console.log(this.$route.params);
+    const id = this.$route.params.id;
+    tagListmodel.fetch();
+    const tags=tagListmodel.data;
+    const tag = tags.filter(t=>t.id===id)[0]
+    if(tag){
+        console.log(tag);
+    }else{
+        this.$router.replace('/404')
+    }
+      
+  }  
 }
 </script>
 <style lang="scss" scoped>
