184436e0f1ea2961a06f8115fabb0041158b8264
luyandan <2103029766@qq.com>
2021-05-11 15:26:35 +0800

    实现编辑和删除标签功能

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -7,6 +7,8 @@ type TagListmodel = {
     data: Tag[]
     fetch: ()=>Tag[]
     create:(name:string) => 'susscess' | 'duplicated'
+    update: (id: string,name: string)=> 'susscess' | 'not found' | 'duplicated'
+    remove: (id: string) => boolean
     save: () => void
 }
 const tagListmodel: TagListmodel = {
@@ -22,6 +24,34 @@ const tagListmodel: TagListmodel = {
      this.data.push({id: name, name: name});
      this.save();
      return 'susscess'
+    },
+    update(id,name){
+        const idList = this.data.map(item => item.id);
+        if(idList.indexOf(id) >= 0){
+            const names = this.data.map(item => item.name);
+            if(names.indexOf(name) >= 0){
+                return 'duplicated'
+            }else{
+                const tag = this.data.filter(item => item.id === id)[0]
+                tag.name = name
+                this.save();
+                return 'susscess'
+            }
+        }else {
+            return 'not found';
+        }
+    },
+    remove(id: string){
+     let index = -1
+     for(let i=0;i<this.data.length;i++){
+         if(this.data[i].id === id){
+             index = i;
+             break;
+         }
+     }
+     this.data.splice(index,1);
+     this.save();
+     return true
     },
     save() {
         window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
