4ce1c8512cad22b1029283b1d051015a367c8175
luyandan <2103029766@qq.com>
2021-05-11 13:46:03 +0800

    使Notes成为通用组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,13 +1,21 @@
 <template>
     <Layout>
-        编辑标签
+        <div>
+           <Icon name="left"/>
+           <span>编辑标签</span>      
+        </div>
+        <Notes field-name="标签名" placeholder="请输入标签名"/>
     </Layout>
 </template>
 <script lang="ts">
-import tagListmodel from '@/models/tagListModel';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-@Component
+import tagListmodel from '@/models/tagListModel';
+import Notes from '@/components/Money/Notes.vue'
+
+@Component({
+  components: {Notes}
+})
 export default class EditLabel extends Vue {
   created() {
     //   console.log(this.$route.params);
