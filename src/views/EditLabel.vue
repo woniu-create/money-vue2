b91346fac3638f5c97aeceb835b06b4ca90a2a61
luyandan <2103029766@qq.com>
2021-05-12 22:32:32 +0800

    EditLabel中的findTag使用Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -18,17 +18,20 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import FormItem from "@/components/Money/FormItem.vue";
 import Button from "@/components/Button.vue";
-// import store from '@/store/index2';
 
 @Component({
   components: { Button, FormItem },
 })
 export default class EditLabel extends Vue {
   // eslint-disable-next-line no-undef
-  tag?: Tag = undefined
+  get tag(){
+    return this.$store.state.currentTag;
+  }
   created() {
     //TODO
     // this.tag = store.findTag(this.$route.params.id);
+    const id = this.$route.params.id;
+    this.$store.commit('setCurrentTag',id); 
     if(!this.tag){
       this.$router.replace('/404');
     }
