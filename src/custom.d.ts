82d1acab79922b8781b4e3960452019ab465053a
luyandan <2103029766@qq.com>
2021-05-12 09:06:55 +0800

    封装record操作

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -24,4 +24,6 @@ interface Window {
     findTag: (id: string) => Tag | undefined
     removeTag: (id: string) => boolean
     updateTag: (id: string,name: string)=> 'susscess' | 'not found' | 'duplicated'
+    recordList: RecordItem[];
+    createRecord: (record: RecordItem) => void
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,11 +20,7 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem  from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListmodel from '@/models/recordListmodel';
-  import tagListmodel from '@/models/tagListModel';
-
-  const recordList = recordListmodel.fetch();
+  import {Component} from 'vue-property-decorator';
 
   @Component({
     components: {Tags, FormItem, Types, NumberPad}
@@ -32,7 +28,7 @@
   export default class Money extends Vue {
     tags = window.tagList 
     // eslint-disable-next-line no-undef
-    recordList: RecordItem[] = recordList;
+    recordList = window.recordList;
     // eslint-disable-next-line no-undef
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
@@ -47,14 +43,7 @@
     }
 
     saveRecord() {
-      // eslint-disable-next-line no-undef
-      recordListmodel.create(this.record);
-    }
-
-    @Watch('recordList')
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    onRecordListChange() {
-      recordListmodel.save();
+     window.createRecord(this.record)
     }
   }
 </script>
